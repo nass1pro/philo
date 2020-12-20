41306bd3b685c12a72e9a0e20ce280983df1e876
nass1pro <nass1pro@gmail.com>
2020-12-20 10:01:06 +0100

    norme-00

--- philo_two/fork.c
+++ philo_two/fork.c
@@ -6,7 +6,7 @@
 /*   By: nahaddac <nahaddac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 14:28:19 by nahaddac          #+#    #+#             */
-/*   Updated: 2020/12/18 14:36:43 by nahaddac         ###   ########.fr       */
+/*   Updated: 2020/12/20 09:59:49 by nahaddac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 int		take_fork(t_philo *philo)
 {
 	if  (sem_wait(philo->argg->fork))
-		return 1;
+		return (1);
 	if (out_message(TYPE_FORK, philo))
-		return  1;
+		return (1);
 	if (sem_wait(philo->argg->fork))
-		return 1;
+		return (1);
 	if (out_message(TYPE_FORK, philo))
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
 
 int		clean_fork(t_philo *philo)
 {
 	if  (sem_post(philo->argg->fork))
-		return 1;
+		return (1);
 	if (sem_post(philo->argg->fork))
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
