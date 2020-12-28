c1558dd880ba20d8bca1366acd57360910dfe3de
nass1pro <nass1pro@gmail.com>
2020-12-28 12:34:30 +0100

    norme forkk

--- philo_two/fork.c
+++ philo_two/fork.c
@@ -6,13 +6,13 @@
 /*   By: nahaddac <nahaddac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 14:28:19 by nahaddac          #+#    #+#             */
-/*   Updated: 2020/12/28 12:30:50 by nahaddac         ###   ########.fr       */
+/*   Updated: 2020/12/28 12:34:12 by nahaddac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo2.h"
 
-int		take_fork(t_philo *philo)
+int			take_fork(t_philo *philo)
 {
 	if (sem_wait(philo->argg->fork))
 		return (1);
@@ -25,7 +25,7 @@ int		take_fork(t_philo *philo)
 	return (0);
 }
 
-int		clean_fork(t_philo *philo)
+int			clean_fork(t_philo *philo)
 {
 	if (sem_post(philo->argg->fork))
 		return (1);
@@ -33,7 +33,6 @@ int		clean_fork(t_philo *philo)
 		return (1);
 	return (0);
 }
-
 
 void		*end_prog_sem(t_philo *philo, int type)
 {
