e01ae0376901c347802caf9fb7942e9fa39039a8
nass1pro <nass1pro@gmail.com>
2021-01-09 04:13:38 +0100

    iiiiii

--- philo_two/main.c
+++ philo_two/main.c
@@ -6,7 +6,7 @@
 /*   By: nahaddac <nahaddac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 13:56:13 by nahaddac          #+#    #+#             */
-/*   Updated: 2021/01/09 04:08:34 by nahaddac         ###   ########.fr       */
+/*   Updated: 2021/01/09 04:13:13 by nahaddac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ void				*philo_life(void *philo)
 	t_philo		*phi;
 
 	phi = (t_philo *)philo;
-	sem_wait(phi->argg->sem_start);
+	// sem_wait(phi->argg->sem_start);
 	phi->last_aet = get_time();
 	phi->current = get_time() - phi->last_aet;
 	phi->limit = phi->last_aet + phi->argg->time_to_die;
@@ -109,9 +109,9 @@ int					philo_create(t_targ *arg)
 	while (i < arg->nb_ph)
 		arg->philo[i++].c_start = get_time();
 	i = -1;
-	while (++i < arg->nb_ph)
-		sem_wait(arg->sem_start);
-	i = -1;
+	// while (++i < arg->nb_ph)
+	// 	sem_wait(arg->sem_start);
+	// i = -1;
 	if (pthread_create(&flush, NULL, &monitor_flush, arg) != 0)
 		return (1);
 	pthread_detach(flush);
@@ -128,8 +128,8 @@ int					philo_create(t_targ *arg)
 	pthread_detach(tid);
 	arg->start = get_time();
 	i = -1;
-	while(++i < arg->nb_ph)
-		sem_post(arg->sem_start);
+	// while(++i < arg->nb_ph)
+	// 	sem_post(arg->sem_start);
 	return (0);
 }
 
